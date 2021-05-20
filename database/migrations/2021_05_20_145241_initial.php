49cf3ce272397e86facb9b10d74641e403391abf
The Matrix <matrix1220@mail.ru>
2021-05-20 20:11:16 +0500

    initial migration

---
+++ database/migrations/2021_05_20_145241_initial.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Initial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->id();
+            $table->string('address');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('collection_id')->nullable();
+
+            $table->string('name');
+            $table->text('description');
+            $table->multiLineString('images');
+            $table->string('brand');
+            $table->float('price', 8, 2);
+
+            $table->integer('available');
+            $table->boolean('is_published');
+            $table->boolean('is_featured');
+
+            $table->timestamps();
+        });
+
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->boolean('is_featured');
+            $table->timestamps();
+        });
+
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('collections');
+        Schema::dropIfExists('banners');
+    }
+}
