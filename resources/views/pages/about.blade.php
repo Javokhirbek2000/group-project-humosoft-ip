731ad34a64910ff911891efc13f530540c886ee7
ShohrukhmirzoGulomov <shohruxgulomov.udemy@gmail.com>
2021-05-21 16:51:26 +0500

    Brend part has been cut

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -47,32 +47,32 @@
             <div class="row">
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img width="200" height="200"  class="img-fluid mb-3 rounded-circle" src="img/Future.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Azizbek Urmonov</h2>
+                    <h2 class="fs-4 font-weight-semi-bold mb-0">Azizbek Urmonov</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img  width="200" height="200" class="img-fluid mb-3 rounded-circle" src="img/shahzod.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Shakhzod Mavlonov</h2>
+                    <h2 class=fs-4 font-weight-semi-bold mb-0">Shakhzod Mavlonov</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img class="img-fluid mb-3 rounded-circle" src="img/matrix.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Khalimetov Mukhiddin</h2>
+                    <h2 class=fs-4 font-weight-semi-bold mb-0">Khalimetov Mukhiddin</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img class="img-fluid mb-3 rounded-circle" src="img/shohruh.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Gulomov Shokhrukhmirzo</h2>
+                    <h2 class=fs-4 font-weight-semi-bold mb-0">Gulomov Shokhrukhmirzo</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img class="img-fluid mb-3 rounded-circle" src="img/javohir.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Mirzaakbarov Javokhirbek</h2>
+                    <h2 class=fs-4 font-weight-semi-bold mb-0">Mirzaakbarov Javokhirbek</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
                 <div class="col-md-4 mb-5 mb-xl-0 col-xl text-center">
                     <img class="img-fluid mb-3 rounded-circle" src="img/komiljon.jpg" alt="Card image cap">
-                    <h2 class="font-size-18 font-weight-semi-bold mb-0">Yangiboev Komiljon</h2>
+                    <h2 class=fs-4 font-weight-semi-bold mb-0">Yangiboev Komiljon</h2>
                     <span class="text-gray-41">Web Developer</span>
                 </div>
             </div>
@@ -244,65 +244,6 @@
                 </div>
             </div>
         </div>
-        <!-- Brand Carousel -->
-        <div class="mb-8">
-            <div class="py-2 border-top border-bottom">
-                <div class="js-slick-carousel u-slick my-1"
-                    data-slides-show="5"
-                    data-slides-scroll="1"
-                    data-arrows-classes="d-none d-lg-inline-block u-slick__arrow-normal u-slick__arrow-centered--y"
-                    data-arrow-left-classes="fa fa-angle-left u-slick__arrow-classic-inner--left z-index-9"
-                    data-arrow-right-classes="fa fa-angle-right u-slick__arrow-classic-inner--right"
-                    data-responsive='[{
-                        "breakpoint": 992,
-                        "settings": {
-                            "slidesToShow": 2
-                        }
-                    }, {
-                        "breakpoint": 768,
-                        "settings": {
-                            "slidesToShow": 1
-                        }
-                    }, {
-                        "breakpoint": 554,
-                        "settings": {
-                            "slidesToShow": 1
-                        }
-                    }]'>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img1.png" alt="Image Description">
-                        </a>
-                    </div>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img2.png" alt="Image Description">
-                        </a>
-                    </div>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img3.png" alt="Image Description">
-                        </a>
-                    </div>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img4.png" alt="Image Description">
-                        </a>
-                    </div>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img5.png" alt="Image Description">
-                        </a>
-                    </div>
-                    <div class="js-slide">
-                        <a href="#" class="link-hover__brand">
-                            <img class="img-fluid m-auto max-height-50" src="../../assets/img/200X60/img6.png" alt="Image Description">
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Brand Carousel -->
     </div>
 </section>
         <!-- ========== END MAIN CONTENT ========== -->
